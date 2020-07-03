32d55622ed765374fb1ac8deaa9b1fa2764e641d
unknown <178430166@qq.com>
2020-07-03 20:15:33 +0800

    uni-app的demo代码

---
+++ pages/component/scroll-view/scroll-view.vue
@@ -0,0 +1,82 @@
+<template>
+	<view>
+		<page-head title="scroll-view,区域滚动视图"></page-head>
+		<view class="uni-margin-wrap">
+			<swiper class="swiper" circular :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+				<swiper-item>
+					<view class="swiper-item uni-bg-red">A</view>
+				</swiper-item>
+				<swiper-item>
+					<view class="swiper-item uni-bg-green">B</view>
+				</swiper-item>
+				<swiper-item>
+					<view class="swiper-item uni-bg-blue">C</view>
+				</swiper-item>
+			</swiper>
+		</view>
+		
+	</view>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				background: ['color1', 'color2', 'color3'],
+				indicatorDots: true,
+				autoplay: true,
+				interval: 2000,
+				duration: 500
+			}
+		},
+	methods: {
+		changeIndicatorDots(e) {
+			this.indicatorDots = !this.indicatorDots
+		},
+		changeAutoplay(e) {
+			this.autoplay = !this.autoplay
+		},
+		intervalChange(e) {
+			this.interval = e.detail.value
+		},
+		durationChange(e) {
+			this.duration = e.detail.value
+		}
+	}
+	}
+</script>
+
+<style>
+	.uni-margin-wrap {
+		width:690rpx;
+		margin:0 30rpx;
+	}
+	.swiper {
+		height: 300rpx;
+	}
+	.swiper-item {
+		display: block;
+		height: 300rpx;
+		line-height: 300rpx;
+		text-align: center;
+	}
+	
+	.swiper-list {
+		margin-top: 40rpx;
+		margin-bottom: 0;
+	}
+	
+	.uni-common-mt{
+		margin-top:60rpx;
+		position:relative;
+	}
+	
+	.info {
+		position: absolute;
+		right:20rpx;
+	}
+	
+	.uni-padding-wrap {
+	    width:550rpx;
+	    padding:0 100rpx;
+	}
+</style>
